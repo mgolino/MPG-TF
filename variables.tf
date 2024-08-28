a948423d4d0b08c7ac48b97e00d6f80ae71668fd
Mark Golino <markvpr@gmail.com>
2024-08-28 15:26:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
-variable "vpc_id" {
-  description = "VPC ID to use if not creating VPC."
-  default     = "vpc-0f72d0fcb34f25235"
-  type        = string
-}
